fd5a08b20606148b3d7e1f068de8b45bafc4fbc5
Ronaldo Torres <rtorres66@misena.edu.co>
2024-11-13 23:20:38 -0500

    Feat: Deploy for demo

--- js/app.js
+++ js/app.js
@@ -2,171 +2,114 @@
 const catalogo = [
     {
         id: 1,
-        categoria: "Anillos",
+        categoria: "Agendas",
         icon: "fas fa-beer",
         products: [
             {
                 id: 1,
-                name: "Anillo 1",
-                description: "Anillo cool",
+                name: "Agenda Bambú",
+                description: "Elaborada en bambú, hojas rayadas, argollada incluye esfero Ecológico.",
                 price: 3.50,
-                image: "https://images.pexels.com/photos/265916/pexels-photo-265916.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            },
-            {
-                id: 2,
-                name: "Anillo 2",
-                description: "Anillo para bodas",
-                price: 3.50,
-                image: "https://images.pexels.com/photos/916344/pexels-photo-916344.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            }
-        ]
-    },
-    {
-        id: 2,
-        categoria: "Cadenas",
-        icon: "fas fa-beer",
-        products: [
-            {
-                id: 1,
-                name: "Cadena de oro",
-                description: "Cadena de oro laminado",
-                price: 3.50,
-                image: "https://images.pexels.com/photos/8706554/pexels-photo-8706554.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                details: "ANCHO: 12.2cm - ALTO: 15 .9cm",
+                image: "/assets/img/agendabambu.png"
             }
         ]
     },
     {
         id: 3,
-        categoria: "Zapatos",
+        categoria: "Hogar",
         icon: "fas fa-beer",
         products: [
             {
                 id: 1,
-                name: "Zapatos Blancos",
-                description: "Zapatos blancos de campo",
+                name: "Asta para Bandera",
+                description: "Asta para colocar banderas en la puerta del carro, no incluye bandera. Medida de Bandera 21*30 cms",
                 price: 3.50,
-                image: "https://images.pexels.com/photos/27095802/pexels-photo-27095802/free-photo-of-piernas-patas-campo-arboles.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            }
-        ]
-    },
-    {
-        id: 4,
-        categoria: "Reloj's",
-        icon: "fas fa-beer",
-        products: [
-            {
-                id: 1,
-                name: "Reloj de oro",
-                description: "Reloj de oro para medir el tiempo",
-                price: 3.50,
-                image: "https://images.pexels.com/photos/277290/pexels-photo-277290.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            }
-        ]
-    },
-    {
-        id: 5,
-        categoria: "Camisas",
-        icon: "fas fa-beer",
-        products: [
-            {
-                id: 1,
-                name: "Camisa de rayas",
-                description: "Camisa de rayas para bodas",
-                price: 3.50,
-                image: "https://images.pexels.com/photos/2635315/pexels-photo-2635315.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                details: "ANCHO: 8cm - LARGO: 28cm",
+                image: "/assets/img/astaparabandera.png"
             }
         ]
     }
-]
+];
+
+// Número de WhatsApp
+const phone = "573175746188"; // Reemplaza con tu número de WhatsApp
+
+// Función para crear la tarjeta de producto
+function crearTarjetaProducto(producto) {
+    // Generar el mensaje para WhatsApp
+    const message = `¡Hola! Estoy interesado en el producto:
+- *Nombre*: ${producto.name}
+- *Precio*: $${producto.price}
+- *Detalles*: ${producto.details}
+Puedes contactarme al siguiente número de WhatsApp: ${phone}`;
+    const whatsappUrl = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
+
+    // Crear la tarjeta de producto
+    const productCard = document.createElement('div');
+    productCard.classList.add('col-md-4');
+    productCard.innerHTML = `
+        <div class="card mb-4 product-wap rounded-0">
+            <div class="card rounded-0">
+                <img class="card-img rounded-0 img-fluid" src="${producto.image}">
+                <div class="card-img-overlay rounded-0 product-overlay d-flex align-items-center justify-content-center">
+                    <ul class="list-unstyled">
+                        <li><a class="btn btn-success text-white" href="${whatsappUrl}" target="_blank"><i class="fa-brands fa-whatsapp"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="card-body">
+                <a class="h3 text-decoration-none fw-bold">${producto.name}</a>
+                <p class="mb-0">${producto.description}</p>
+                <hr>
+                <p class="mb-0">${producto.details}</p>
+                <hr>
+                <p class="text-center mb-0 fw-bold">Precio: $${producto.price}</p>
+            </div>
+        </div>
+    `;
+    return productCard;
+}
 
 // Función para inicializar la aplicación y generar la lista de categorías
 function iniciarApp() {
-
-    //mostrar todos los productos
     const productsList = document.getElementById('products-list');
     const allProducts = catalogo.map(product => product.products).flat();
+
+    // Mostrar todos los productos
     allProducts.forEach(producto => {
-        const productCard = document.createElement('div');
-        productCard.classList.add('col-md-4');
-
-        productCard.innerHTML = `
-            <div class="card mb-4 product-wap rounded-0">
-                <div class="card rounded-0">
-                    <img class="card-img rounded-0 img-fluid" src="${producto.image}">
-                    <div class="card-img-overlay rounded-0 product-overlay d-flex align-items-center justify-content-center">
-                        <ul class="list-unstyled">
-                            <li><a class="btn btn-success text-white"><i class="far fa-heart"></i></a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="card-body">
-                    <a class="h3 text-decoration-none">${producto.name}</a>
-                    <p class="text-center mb-0">$${producto.price}</p>
-                </div>
-            </div>
-        `;
-
+        const productCard = crearTarjetaProducto(producto);
         productsList.appendChild(productCard);
     });
 
     const categoryList = document.getElementById('category-list');
-
-    // Obtener las categorías únicas
     const uniqueCategories = catalogo.map(product => product.categoria);
 
     // Crear la lista de categorías
     uniqueCategories.forEach(category => {
         const li = document.createElement('li');
         li.classList.add('pb-3');
-
         li.innerHTML = `
             <a class="collapsed d-flex justify-content-between h3 text-decoration-none" href="#" data-category="${category}">
                 ${category}
             </a>
         `;
-
-        // Agregar un evento click para mostrar los productos de la categoría seleccionada
         li.querySelector('a').addEventListener('click', function (e) {
             e.preventDefault();
-            const selectedCategory = this.getAttribute('data-category');
-            mostrarProductos(selectedCategory);
+            mostrarProductos(this.getAttribute('data-category'));
         });
-
         categoryList.appendChild(li);
     });
 }
 
 // Función para mostrar los productos de una categoría específica
 function mostrarProductos(categoria) {
-
     const productsList = document.getElementById('products-list');
     productsList.innerHTML = ''; // Limpiar productos anteriores
 
-    // Filtrar productos por la categoría seleccionada
     const productosCategoria = catalogo.find(product => product.categoria === categoria).products;
-
-    // Generar las tarjetas de productos
     productosCategoria.forEach(producto => {
-        const productCard = document.createElement('div');
-        productCard.classList.add('col-md-4');
-
-        productCard.innerHTML = `
-            <div class="card mb-4 product-wap rounded-0">
-                <div class="card rounded-0">
-                    <img class="card-img rounded-0 img-fluid" src="${producto.image}">
-                    <div class="card-img-overlay rounded-0 product-overlay d-flex align-items-center justify-content-center">
-                        <ul class="list-unstyled">
-                            <li><a class="btn btn-success text-white"><i class="far fa-heart"></i></a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="card-body">
-                    <a class="h3 text-decoration-none">${producto.name}</a>
-                    <p class="text-center mb-0">$${producto.price}</p>
-                </div>
-            </div>
-        `;
-
+        const productCard = crearTarjetaProducto(producto);
         productsList.appendChild(productCard);
     });
 }
